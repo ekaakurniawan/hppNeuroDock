9538fc8dfb0dfcf107c2e4d7b7dcc7ae4a87e737
Iced Milo <icedmilo@outlook.com>
2013-03-08 12:12:00 +0800

    Support for flexible side-chain is added and tested
    The flexible part is read-in and merged with the ligand, same as how the
    Autodock works.
    setAbout works only on the ligand, not the flex part.
    twsit (torsions applied to rotatable bonds) is applied to all torsions,
    including ligand and the flexible part.
    transform (translation and rotation) is applied to the ligand part only.

--- PyAutoDock/test.sh
+++ PyAutoDock/test.sh
@@ -1,5 +1,5 @@
-python Molecule.py | grep "^HETATM" | cut -c31-54 > log
-sed -n '6696,6744p' Results/ind.dlg| cut -c31-54 > ref
+python Molecule.py | grep "^ATOM" | cut -c31-54 > log
+sed -n '6696,6768p' Results/ind.dlg| cut -c31-54 > ref
 diff log ref
 rm log
 rm ref
